7dd5b85992bf74de065ddfb9fbef5da087541204
Park Sang Min <sm0104224@gmail.com>
2026-06-29 13:19:52 +0900

    chore: update formula to v0.4.1

--- Formula/lazyredis.rb
+++ Formula/lazyredis.rb
@@ -1,26 +1,26 @@
 class Lazyredis < Formula
   desc "Lazygit-inspired terminal UI for Redis"
   homepage "https://github.com/sm010422/lazyredis"
-  version "0.3.0"
+  version "0.4.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/sm010422/lazyredis/releases/download/v0.3.0/lazyredis_darwin_arm64.tar.gz"
-      sha256 "60f829f41ad288ca147d147cb23b798c1c4e25189791484d2d733f6335970fa8"
+      url "https://github.com/sm010422/lazyredis/releases/download/v0.4.1/lazyredis_darwin_arm64.tar.gz"
+      sha256 "0b87773c58aff05fd3a9c2ea695cd1b2aefdfb231217adcc98d07291701f9b5e"
     else
-      url "https://github.com/sm010422/lazyredis/releases/download/v0.3.0/lazyredis_darwin_amd64.tar.gz"
-      sha256 "58a25cc72a67f948505f89f9461ef22bd13d76bd4b4f518b13f17c50ad6464e9"
+      url "https://github.com/sm010422/lazyredis/releases/download/v0.4.1/lazyredis_darwin_amd64.tar.gz"
+      sha256 "51d292aba8e37f2cb637c092129b86635e1256cf4390073102dd1b1ec0467e47"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/sm010422/lazyredis/releases/download/v0.3.0/lazyredis_linux_arm64.tar.gz"
-      sha256 "87bf3c2351845cde19958a2877aabbb401c8d206b95fe585513a687bcaf7f544"
+      url "https://github.com/sm010422/lazyredis/releases/download/v0.4.1/lazyredis_linux_arm64.tar.gz"
+      sha256 "2701d6c91e2532e7c532c0f945f5078fe49136ce69de13aaf628379422232d18"
     else
-      url "https://github.com/sm010422/lazyredis/releases/download/v0.3.0/lazyredis_linux_amd64.tar.gz"
-      sha256 "c3c38a5087b2afffcabba40f63f29a2b545ad0e3978e948c4b8f8db52963bff9"
+      url "https://github.com/sm010422/lazyredis/releases/download/v0.4.1/lazyredis_linux_amd64.tar.gz"
+      sha256 "fbbb72246cf7216212bf0937d639ef60321a0b204ba1a4d19b2786f7f29bc976"
     end
   end
 
@@ -29,7 +29,6 @@ class Lazyredis < Formula
   end
 
   test do
-    # lazyredis requires a TTY, so just check the binary exists and runs
     assert_predicate bin/"lazyredis", :exist?
   end
 end
