c46b9af34dad4740cd0e36948742cf8666a867b9
Park Sang Min <sm0104224@gmail.com>
2026-06-29 13:31:16 +0900

    chore: update formula to v0.4.2

--- Formula/lazyredis.rb
+++ Formula/lazyredis.rb
@@ -1,26 +1,26 @@
 class Lazyredis < Formula
   desc "Lazygit-inspired terminal UI for Redis"
   homepage "https://github.com/sm010422/lazyredis"
-  version "0.4.1"
+  version "0.4.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/sm010422/lazyredis/releases/download/v0.4.1/lazyredis_darwin_arm64.tar.gz"
-      sha256 "0b87773c58aff05fd3a9c2ea695cd1b2aefdfb231217adcc98d07291701f9b5e"
+      url "https://github.com/sm010422/lazyredis/releases/download/v0.4.2/lazyredis_darwin_arm64.tar.gz"
+      sha256 "05f0125f1d4f4d8ca6d74fd707b085feacf7ed20af3f98f7e29e3fdb6442cb81"
     else
-      url "https://github.com/sm010422/lazyredis/releases/download/v0.4.1/lazyredis_darwin_amd64.tar.gz"
-      sha256 "51d292aba8e37f2cb637c092129b86635e1256cf4390073102dd1b1ec0467e47"
+      url "https://github.com/sm010422/lazyredis/releases/download/v0.4.2/lazyredis_darwin_amd64.tar.gz"
+      sha256 "b804fe389ece1aee8d23b149c86050a6fb7f60850611e93826f223ea614b10ec"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/sm010422/lazyredis/releases/download/v0.4.1/lazyredis_linux_arm64.tar.gz"
-      sha256 "2701d6c91e2532e7c532c0f945f5078fe49136ce69de13aaf628379422232d18"
+      url "https://github.com/sm010422/lazyredis/releases/download/v0.4.2/lazyredis_linux_arm64.tar.gz"
+      sha256 "cc168af0c772196e5dadba285da49c25c47ebbb5e375c5ef60d9508f66f8611f"
     else
-      url "https://github.com/sm010422/lazyredis/releases/download/v0.4.1/lazyredis_linux_amd64.tar.gz"
-      sha256 "fbbb72246cf7216212bf0937d639ef60321a0b204ba1a4d19b2786f7f29bc976"
+      url "https://github.com/sm010422/lazyredis/releases/download/v0.4.2/lazyredis_linux_amd64.tar.gz"
+      sha256 "d63b03435028efa3fc3ae8baaf25a45f08f6bf2f1e8b0b22690a5c8b0873f096"
     end
   end
 
